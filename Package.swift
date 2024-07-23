becf1204c7394731703d78c5f8251c66ff8f4eb4
Oleksii Salmin <oleksii.salmin@skelia.com>
2024-07-23 15:01:32 +0200

    fix: pinned all poinfree dependencies to match artemis before dynamic overlay rename.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
-// swift-tools-version:5.7.1
+// swift-tools-version:5.9
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
@@ -19,27 +20,34 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-collections", from: "1.0.2"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
     .package(url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/combine-schedulers", exact: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-case-paths", exact: "1.4.2"),
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.1.0"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "1.1.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", exact: "1.3.0"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", exact: "1.3.3"),
+    .package(url: "https://github.com/pointfreeco/swift-identified-collections", exact: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.0"),
+    .package(url: "https://github.com/pointfreeco/swift-perception", exact: "1.2.4"),
+    .package(url: "https://github.com/pointfreeco/swiftui-navigation", exact: "1.5.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", exact: "1.1.2"),
+    .package(url: "https://github.com/pointfreeco/swift-clocks", exact: "1.0.2"),
   ],
   targets: [
     .target(
       name: "ComposableArchitecture",
       dependencies: [
+        "ComposableArchitectureMacros",
         .product(name: "CasePaths", package: "swift-case-paths"),
         .product(name: "CombineSchedulers", package: "combine-schedulers"),
         .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras"),
         .product(name: "CustomDump", package: "swift-custom-dump"),
         .product(name: "Dependencies", package: "swift-dependencies"),
+        .product(name: "DependenciesMacros", package: "swift-dependencies"),
         .product(name: "IdentifiedCollections", package: "swift-identified-collections"),
         .product(name: "OrderedCollections", package: "swift-collections"),
+        .product(name: "Perception", package: "swift-perception"),
         .product(name: "SwiftUINavigationCore", package: "swiftui-navigation"),
         .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
       ]
@@ -48,6 +56,20 @@ let package = Package(
       name: "ComposableArchitectureTests",
       dependencies: [
         "ComposableArchitecture"
+      ]
+    ),
+    .macro(
+      name: "ComposableArchitectureMacros",
+      dependencies: [
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+      ]
+    ),
+    .testTarget(
+      name: "ComposableArchitectureMacrosTests",
+      dependencies: [
+        "ComposableArchitectureMacros",
+        .product(name: "MacroTesting", package: "swift-macro-testing"),
       ]
     ),
     .executableTarget(
